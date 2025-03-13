b794cad381078f750fd411a211ff36e6b15d7b5b
pstap21 <a1629853@adelaide.edu.au>
2025-03-13 12:30:41 +1030

    Add files via upload

---
+++ functions5.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+
+void print_binary_str(std::string decimal_number);
+
+
+#endif
---
+++ main-2-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "functions5.h"
+
+int main() {
+std::string input;
+std::cout << "Enter decimal number: ";
+std::cin >> input;
+
+print_binary_str(input);
+
+return 0;
+}
